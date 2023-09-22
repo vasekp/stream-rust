c97d5ddb07159f76083ae6194f43a780fa285d48
Vašek Potoček <vasek.potocek@post.cz>
2023-09-22 17:34:52 +0200

    Move executable code to main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 pub mod base;
 pub mod ops;
 
-pub fn lib_main() {
-    let params = vec![base::Item::new_imm(1), base::Item::new_imm(3)];
-    let s = ops::IotaStream::construct(params).unwrap();
-    println!("{:.80}", s);
-}
+pub use base::*;
+pub use ops::*;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use streamlang::lib_main;
+use streamlang::*;
 
 fn main() {
-    lib_main();
+    let params = vec![Item::new_imm(1), Item::new_imm(3)];
+    let s = IotaStream::construct(params).unwrap();
+    println!("{:.80}", s);
 }
