3504ca17e47edb7b4cd436e962e7e8266e3166fd
Vašek Potoček <vasek.potocek@post.cz>
2023-09-30 22:01:36 +0200

    Parser: tokenizer iterator returns &str

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,13 @@ fn main() {
     /*let params = vec![Item::new_imm(1), Item::new_imm(3)];
     let s = RangeStream::construct(params).unwrap();
     println!("{:.80?}", s);*/
-    let tk = Tokenizer::new(r#"aah"ooo"j"#);
+    let input = r#"aah"ooo"j"#;
+    let tk = Tokenizer::new(input);
     for t in tk {
-        println!("{:?}", t);
+        if let Ok(slice) = t {
+            let start = unsafe { slice.as_ptr().offset_from(input.as_ptr()) };
+            let length = slice.len();
+            println!("{slice}: {start} + {length}");
+        }
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -20,7 +20,7 @@ impl<'a> Tokenizer<'a> {
         Tokenizer{input, chars: input.chars().peekable(), index: 0}
     }
 
-    fn read_string(&mut self) -> Option<StreamResult<Token<'a>>> {
+    fn read_string(&mut self) -> Option<StreamResult<&'a str>> {
         let start = self.index;
         self.chars.next();
         let mut length = 1;
@@ -39,12 +39,12 @@ impl<'a> Tokenizer<'a> {
             return Some(Err(StreamError("unterminated string".to_string())));
         }
         self.index = self.index + length;
-        Some(Ok(Token{slice: &self.input[start..self.index], start, length}))
+        Some(Ok(&self.input[start..self.index]))
     }
 }
 
 impl<'a> Iterator for Tokenizer<'a> {
-    type Item = StreamResult<Token<'a>>;
+    type Item = StreamResult<&'a str>;
 
     fn next(&mut self) -> Option<Self::Item> {
         if let Some(&ch) = self.chars.peek() {
@@ -62,7 +62,7 @@ impl<'a> Iterator for Tokenizer<'a> {
                 self.chars.next();
             }
             self.index = self.index + length;
-            Some(Ok(Token{slice: &self.input[start..self.index], start, length}))
+            Some(Ok(&self.input[start..self.index]))
         } else {
             None
         }
@@ -72,37 +72,37 @@ impl<'a> Iterator for Tokenizer<'a> {
 #[test]
 fn test_parser() {
     let mut tk = Tokenizer::new(r#"a""d"#);
-    assert_eq!(tk.next(), Some(Ok(Token{slice: "a", start: 0, length: 1})));
-    assert_eq!(tk.next(), Some(Ok(Token{slice: "\"\"", start: 1, length: 2})));
-    assert_eq!(tk.next(), Some(Ok(Token{slice: "d", start: 3, length: 1})));
+    assert_eq!(tk.next(), Some(Ok("a")));
+    assert_eq!(tk.next(), Some(Ok("\"\"")));
+    assert_eq!(tk.next(), Some(Ok("d")));
     assert_eq!(tk.next(), None);
 
     let mut tk = Tokenizer::new(r#"a"d"#);
-    assert_eq!(tk.next(), Some(Ok(Token{slice: "a", start: 0, length: 1})));
+    assert_eq!(tk.next(), Some(Ok("a")));
     assert_eq!(tk.next(), Some(Err(StreamError("unterminated string".to_string()))));
     assert_eq!(tk.next(), None);
 
     let mut tk = Tokenizer::new(r#"a"""d"#);
-    assert_eq!(tk.next(), Some(Ok(Token{slice: "a", start: 0, length: 1})));
-    assert_eq!(tk.next(), Some(Ok(Token{slice: "\"\"", start: 1, length: 2})));
+    assert_eq!(tk.next(), Some(Ok("a")));
+    assert_eq!(tk.next(), Some(Ok("\"\"")));
     assert_eq!(tk.next(), Some(Err(StreamError("unterminated string".to_string()))));
     assert_eq!(tk.next(), None);
 
     let mut tk = Tokenizer::new(r#"a"\""d"#);
-    assert_eq!(tk.next(), Some(Ok(Token{slice: "a", start: 0, length: 1})));
-    assert_eq!(tk.next(), Some(Ok(Token{slice: "\"\\\"\"", start: 1, length: 4})));
-    assert_eq!(tk.next(), Some(Ok(Token{slice: "d", start: 5, length: 1})));
+    assert_eq!(tk.next(), Some(Ok("a")));
+    assert_eq!(tk.next(), Some(Ok("\"\\\"\"")));
+    assert_eq!(tk.next(), Some(Ok("d")));
     assert_eq!(tk.next(), None);
 
     let mut tk = Tokenizer::new(r#"a"\\"d"#);
-    assert_eq!(tk.next(), Some(Ok(Token{slice: "a", start: 0, length: 1})));
-    assert_eq!(tk.next(), Some(Ok(Token{slice: "\"\\\\\"", start: 1, length: 4})));
-    assert_eq!(tk.next(), Some(Ok(Token{slice: "d", start: 5, length: 1})));
+    assert_eq!(tk.next(), Some(Ok("a")));
+    assert_eq!(tk.next(), Some(Ok("\"\\\\\"")));
+    assert_eq!(tk.next(), Some(Ok("d")));
     assert_eq!(tk.next(), None);
 
     let mut tk = Tokenizer::new(r#"a\"d"#);
-    assert_eq!(tk.next(), Some(Ok(Token{slice: "a", start: 0, length: 1})));
-    assert_eq!(tk.next(), Some(Ok(Token{slice: "\\", start: 1, length: 1})));
+    assert_eq!(tk.next(), Some(Ok("a")));
+    assert_eq!(tk.next(), Some(Ok("\\")));
     assert_eq!(tk.next(), Some(Err(StreamError("unterminated string".to_string()))));
     assert_eq!(tk.next(), None);
 }
