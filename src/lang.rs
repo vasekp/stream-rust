3394c26ad14db14352d2397996b0a6b138109cc4
Vašek Potoček <vasek.potocek@post.cz>
2024-07-22 21:32:46 +0200

    '$part': accept sequences in parts specification

--- src/lang.rs
+++ src/lang.rs
@@ -99,39 +99,109 @@ impl From<String> for LiteralString {
 }
 
 
-fn eval_part(node: Node, env: &Rc<Env>) -> Result<Item, StreamError> {
-    let mut node = node.eval_all(env)?;
-    let source = try_with!(node, node.source_checked());
-    match &node.args[..] {
-        [] => {
-            Err(StreamError::new("at least 1 argument required", node))
-        },
-        [first, ..] => {
-            let index = try_with!(node, first.as_num());
-            try_with!(node, index.check_within(Number::one()..));
-            let stm = try_with!(node, source.to_stream());
-            match stm.length() {
-                Length::Exact(len) | Length::AtMost(len) if &len < index =>
-                    return Err(StreamError::new("index past end of stream", node)),
-                _ => ()
-            }
-            let mut iter = stm.iter();
-            if iter.skip_n(&(index - 1))?.is_some() {
-                return Err(StreamError::new("index past end of stream", node));
-            }
-            let item = match iter.next() {
-                Some(value) => value?,
-                None => return Err(StreamError::new("index past end of stream", node))
-            };
-            node.args.remove(0);
-            if node.args.is_empty() {
-                Ok(item)
-            } else {
-                eval_part(ENode{head: node.head, source: Some(item), args: node.args}.into(), env)
+pub struct Part {
+    source: Box<dyn Stream>,
+    indices: Box<dyn Stream>,
+    rest: Vec<Item>,
+    env: Rc<Env>
+}
+
+impl Part {
+    fn eval(node: Node, env: &Rc<Env>) -> Result<Item, StreamError> {
+        let mut node = node.eval_all(env)?;
+        let source = try_with!(node, node.source_checked()?.to_stream());
+        match node.args.get(0) {
+            None => {
+                Err(StreamError::new("at least 1 argument required", node))
+            },
+            Some(first) => {
+                match first {
+                    Item::Number(index) => {
+                        try_with!(node, index.check_within(Number::one()..));
+                        match source.length() {
+                            Length::Exact(len) | Length::AtMost(len) if &len < index =>
+                                return Err(StreamError::new("index past end of stream", node)),
+                            _ => ()
+                        }
+                        let mut iter = source.iter();
+                        if iter.skip_n(&(index - 1))?.is_some() {
+                            return Err(StreamError::new("index past end of stream", node));
+                        }
+                        let item = match iter.next() {
+                            Some(value) => value?,
+                            None => return Err(StreamError::new("index past end of stream", node))
+                        };
+                        node.args.remove(0);
+                        if node.args.is_empty() {
+                            Ok(item)
+                        } else {
+                            Part::eval(ENode{head: node.head, source: Some(item), args: node.args}.into(), env)
+                        }
+                    },
+                    Item::Stream(_) => {
+                        let indices = node.args.remove(0).to_stream().unwrap();
+                        Ok(Item::new_stream(Part{source, indices, rest: node.args, env: Rc::clone(env)}))
+                    },
+                    _ => Err(StreamError::new(format!("expected number or stream, found {:?}", first), node))
+                }
             }
         }
     }
 }
+
+impl Stream for Part {
+    fn iter<'node>(&'node self) -> Box<dyn SIterator + 'node> {
+        Box::new(PartIter{parent: self, iter: self.indices.iter()})
+    }
+}
+
+impl Describe for Part {
+    fn describe(&self) -> String {
+        let mut args = self.rest.clone();
+        let source = self.source.to_item();
+        args.insert(0, self.indices.to_item());
+        Node::describe_helper(&Head::Lang(LangItem::Part), Some(&source), &args)
+    }
+}
+
+impl Clone for Part {
+    fn clone(&self) -> Part {
+        Part {
+            source: self.source.clone_box(),
+            indices: self.indices.clone_box(),
+            rest: self.rest.clone(),
+            env: Rc::clone(&self.env)
+        }
+    }
+}
+
+struct PartIter<'node> {
+    parent: &'node Part,
+    iter: Box<dyn SIterator + 'node>
+}
+
+impl Iterator for PartIter<'_> {
+    type Item = Result<Item, StreamError>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let part = self.iter.next()?;
+        let Ok(part) = part else {
+            return Some(part);
+        };
+        // TODO: smarter - number tracks increments, stream unfolds?
+        let mut args = self.parent.rest.clone();
+        args.insert(0, part);
+        let node = ENode{head: LangItem::Part.into(), source: Some(self.parent.source.to_item()), args};
+        Some(Part::eval(node.into(), &self.parent.env))
+    }
+}
+
+impl SIterator for PartIter<'_> {
+    fn skip_n(&mut self, n: &Number) -> Result<Option<Number>, StreamError> {
+        self.iter.skip_n(n)
+    }
+}
+
 
 #[test]
 fn test_part() {
@@ -144,6 +214,19 @@ fn test_part() {
     assert!(parse("range(3)[-1]").unwrap().eval().is_err());
     assert_eq!(parse("[[1,2],[3,4]][2,1]").unwrap().eval().unwrap().to_string(), "3");
     assert_eq!(parse("[[1,2],[3,4]][2][1]").unwrap().eval().unwrap().to_string(), "3");
+
+    assert_eq!(parse("seq(5,2)[100...]").unwrap().eval().unwrap().to_string(), "[203, 203, 203, ...");
+    assert_eq!(parse("seq(5,2)[2*seq+1]").unwrap().eval().unwrap().to_string(), "[9, 13, 17, ...");
+    assert_eq!(parse("seq[seq][seq]").unwrap().eval().unwrap().to_string(), "[1, 2, 3, ...");
+    assert_eq!(parse("seq[seq, seq]").unwrap().eval().unwrap().to_string(), "[<!>");
+    assert_eq!(parse("seq:{seq^#}[seq,4]").unwrap().eval().unwrap().to_string(), "[4, 16, 64, ...");
+    assert_eq!(parse("seq:{seq^#}[seq][4]").unwrap().eval().unwrap().to_string(), "[1, 16, 81, ...");
+    assert_eq!(parse("seq:{seq^#}[4,seq]").unwrap().eval().unwrap().to_string(), "[1, 16, 81, ...");
+    assert_eq!(parse("seq:{seq^#}[4][seq]").unwrap().eval().unwrap().to_string(), "[1, 16, 81, ...");
+    assert_eq!(parse("seq:{seq^#}[[1,2],[1,2,3]]").unwrap().eval().unwrap().to_string(), "[[1, 2, 3], [1, 4, 9]]");
+    assert!(parse("seq[2,5]").unwrap().eval().is_err());
+    assert_eq!(parse("seq[[2,5]]").unwrap().eval().unwrap().to_string(), "[2, 5]");
+    assert_eq!(parse("seq[[[2,5]]]").unwrap().eval().unwrap().to_string(), "[[2, 5]]"); // subject to change
 }
 
 
@@ -658,7 +741,7 @@ fn test_args() {
 
 pub(crate) fn init(keywords: &mut crate::keywords::Keywords) {
     keywords.insert("$list", List::eval);
-    keywords.insert("$part", eval_part);
+    keywords.insert("$part", Part::eval);
     keywords.insert("$map", Map::eval);
     keywords.insert("$args", eval_args);
     keywords.insert("+", MathOp::eval);
