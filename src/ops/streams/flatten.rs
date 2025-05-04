4353deccb9da7eecb3e308bd15091522af5210d5
Vašek Potoček <vasek.potocek@post.cz>
2025-05-05 00:17:09 +0200

    'flatten': depth argument

--- src/ops/streams/flatten.rs
+++ src/ops/streams/flatten.rs
@@ -3,6 +3,7 @@ use crate::base::*;
 #[derive(Clone)]
 struct Flatten {
     source: BoxedStream,
+    depth: Option<UNumber>,
     head: Head
 }
 
@@ -10,22 +11,29 @@ impl Flatten {
     fn eval(node: Node, env: &Rc<Env>) -> Result<Item, StreamError> {
         match node.eval_all(env)?.resolve_source()? {
             RNodeS { head, source: Item::Stream(stm), args: RArgs::Zero } => {
-                Ok(Item::new_stream(Flatten { source: stm.into(), head }))
+                Ok(Item::new_stream(Flatten { source: stm.into(), head, depth: None }))
             },
-            node => Err(StreamError::new("expected: stream.flatten", node))
+            RNodeS { head, source: Item::Stream(stm), args: RArgs::One(Item::Number(depth)) } if !depth.is_negative() => {
+                Ok(Item::new_stream(Flatten { source: stm.into(), head, depth: Some(crate::utils::unsign(depth)) }))
+            },
+            node => Err(StreamError::new("expected: stream.flatten or stream.flatten(depth)", node))
         }
     }
 }
 
 impl Describe for Flatten {
     fn describe_prec(&self, prec: u32) -> String {
-        Node::describe_helper(&self.head, Some(&self.source), None::<&Item>, prec)
+        Node::describe_helper(&self.head, Some(&self.source), self.depth.as_ref(), prec)
     }
 }
 
 impl Stream for Flatten {
     fn iter<'node>(&'node self) -> Box<dyn SIterator + 'node> {
-        Box::new(FlattenIter{outer: self.source.iter(), inner: None})
+        Box::new(FlattenIter {
+            outer: self.source.iter(),
+            iters: vec![],
+            depth: self.depth.as_ref().and_then(UNumber::to_usize),
+        })
     }
 
     fn length(&self) -> Length {
@@ -43,7 +51,8 @@ impl Stream for Flatten {
 
 struct FlattenIter<'node> {
     outer: Box<dyn SIterator + 'node>,
-    inner: Option<OwnedStreamIter<'node>>
+    iters: Vec<OwnedStreamIter<'node>>,
+    depth: Option<usize>,
 }
 
 impl Iterator for FlattenIter<'_> {
@@ -51,20 +60,26 @@ impl Iterator for FlattenIter<'_> {
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
-            if let Some(inner) = &mut self.inner {
-                let next = inner.next();
-                if next.is_some() {
-                    return next;
-                } else {
-                    self.inner = None;
-                }
-            }
-            match self.outer.next()? {
-                Ok(Item::Stream(stm)) => {
-                    self.inner = Some(stm.into());
-                    continue;
+            let res = match self.iters.last_mut() {
+                Some(iter) => iter.next(),
+                None => self.outer.next()
+            };
+            match res {
+                Some(Ok(Item::Stream(stm))) => {
+                    if self.depth.is_some_and(|d| self.iters.len() == d) {
+                        return Some(Ok(Item::Stream(stm)));
+                    } else {
+                        self.iters.push(OwnedStreamIter::from(stm));
+                    }
                 },
-                res => return Some(res)
+                Some(res) => return Some(res),
+                None => {
+                    if self.iters.is_empty() {
+                        return None;
+                    } else {
+                        self.iters.pop();
+                    }
+                }
             }
         }
     }
@@ -73,28 +88,33 @@ impl Iterator for FlattenIter<'_> {
 impl SIterator for FlattenIter<'_> {
     fn skip_n(&mut self, mut n: UNumber) -> Result<Option<UNumber>, StreamError> {
         loop {
-            println!("{n:?}");
-            if let Some(inner) = &mut self.inner {
-                let Some(m) = inner.skip_n(n)?
-                    else { return Ok(None); };
-                self.inner = None;
-                n = m;
-            }
             if n.is_zero() {
                 return Ok(None);
             }
-            match self.outer.next() {
-                Some(Ok(Item::Stream(stm))) => {
-                    self.inner = Some(stm.into());
-                },
-                Some(Ok(_)) => {
-                    n.dec();
-                    if n.is_zero() {
-                        return Ok(None);
+            if self.depth.is_some_and(|d| self.iters.len() == d) {
+                let Some(iter) = self.iters.last_mut() else { unreachable!() };
+                let Some(m) = iter.skip_n(n)? else { return Ok(None); };
+                self.iters.pop();
+                n = m;
+            } else {
+                let res = match self.iters.last_mut() {
+                    Some(iter) => iter.next(),
+                    None => self.outer.next()
+                };
+                match res {
+                    Some(Ok(Item::Stream(stm))) => {
+                        self.iters.push(OwnedStreamIter::from(stm));
+                    },
+                    Some(Ok(_)) => n.dec(),
+                    Some(Err(err)) => return Err(err),
+                    None => {
+                        if self.iters.is_empty() {
+                            return Ok(Some(n));
+                        } else {
+                            self.iters.pop();
+                        }
                     }
-                },
-                Some(Err(err)) => return Err(err),
-                None => return Ok(Some(n))
+                }
             }
         }
     }
@@ -112,8 +132,16 @@ mod tests {
     fn test_flatten() {
         use crate::parser::parse;
 
-        assert_eq!(parse("[1, [2, [3]]].flatten").unwrap().eval().unwrap().to_string(), "[1, 2, [3]]");
+        assert_eq!(parse("[1, [2, [3]]].flatten").unwrap().eval().unwrap().to_string(), "[1, 2, 3]");
+        assert_eq!(parse("[1, [2, [3]]].flatten(1)").unwrap().eval().unwrap().to_string(), "[1, 2, [3]]");
+        assert_eq!(parse("[0].nest{[#]}.flatten").unwrap().eval().unwrap().to_string(), "[0, 0, 0, 0, 0, ...]");
+        assert_eq!(parse("[0].nest{[#]}.flatten(3)").unwrap().eval().unwrap().to_string(), "[0, 0, [0], [...], ...]");
+        assert_eq!(parse("[0].nest{[#]}.flatten(10^10)").unwrap().eval().unwrap().to_string(), "[0, 0, 0, 0, 0, ...]");
         test_skip_n(&parse("[1,range(3)].repeat(10).flatten").unwrap().eval().unwrap());
+        test_skip_n(&parse("[1,[2,[3]]].repeat(10).flatten").unwrap().eval().unwrap());
+        test_skip_n(&parse("[1,[2,[3]]].repeat(10).flatten(1)").unwrap().eval().unwrap());
+        test_skip_n(&parse("[1,[2,[3]]].repeat(10).flatten(2)").unwrap().eval().unwrap());
+        test_skip_n(&parse("[1,[2,[3]]].repeat(10).flatten(10)").unwrap().eval().unwrap());
     }
 }
 
