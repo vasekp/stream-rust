4c8e85c70a2349015c90009012ceb3aa3ff29794
Vašek Potoček <vasek.potocek@post.cz>
2025-06-02 09:23:27 +0200

    'islower', 'isupper' for strings, new op 'isnumeric'

--- src/ops/cond/class.rs
+++ src/ops/cond/class.rs
@@ -35,8 +35,65 @@ fn eval_inner(head: &str, item: &Item, env: &Env) -> Result<bool, BaseError> {
             Char::Single(ch) => ch.is_whitespace(),
             _ => false
         }),
-        "isupper" => Ok(item.as_char()?.case() == CharCase::Upper),
-        "islower" => Ok(item.as_char()?.case() == CharCase::Lower),
+        "isupper" => Ok(match item {
+            Item::Char(ch) => ch.case() == CharCase::Upper,
+            Item::String(stm) => {
+                let mut indet = true;
+                for ch in stm.string_iter() {
+                    check_stop!();
+                    let ch = ch?;
+                    match ch.case() {
+                        CharCase::Lower | CharCase::Mixed => return Ok(false),
+                        CharCase::Upper => indet = false,
+                        CharCase::Indeterminate => ()
+                    }
+                }
+                !indet
+            },
+            _ => return Err(format!("expected character or string, found {:?}", item).into())
+        }),
+        "islower" => Ok(match item {
+            Item::Char(ch) => ch.case() == CharCase::Lower,
+            Item::String(stm) => {
+                let mut indet = true;
+                for ch in stm.string_iter() {
+                    check_stop!();
+                    let ch = ch?;
+                    match ch.case() {
+                        CharCase::Upper | CharCase::Mixed => return Ok(false),
+                        CharCase::Lower => indet = false,
+                        CharCase::Indeterminate => ()
+                    }
+                }
+                !indet
+            },
+            _ => return Err(format!("expected character or string, found {:?}", item).into())
+        }),
+        "isnumeric" => {
+            if let Item::Char(ch) = item {
+                return match ch {
+                    Char::Single(ch) => Ok(ch.is_ascii_digit()),
+                    _ => Ok(false)
+                }
+            }
+            let Item::String(stm) = item else { return Err(format!("expected character or string, found {:?}", item).into()); };
+            let mut iter = stm.string_iter();
+            let mut nonempty = match iter.next().transpose()? {
+                Some(ch) => match ch {
+                    Char::Single('+'|'-') => false,
+                    Char::Single(x) if x.is_ascii_digit() => true,
+                    _ => return Ok(false)
+                },
+                None => return Ok(false)
+            };
+            for ch in iter {
+                match ch? {
+                    Char::Single(x) if x.is_ascii_digit() => nonempty = true,
+                    _ => return Ok(false)
+                }
+            }
+            Ok(nonempty)
+        },
         _ => unimplemented!()
     }
 }
@@ -60,8 +117,13 @@ mod tests {
         assert_eq!(parse("['a','á','ch',\"a\"]:isascii").unwrap().eval_default().unwrap().to_string(), "[true, false, false, <!>");
         assert_eq!(parse("['0','00',\"0\"]:isdigit").unwrap().eval_default().unwrap().to_string(), "[true, false, <!>");
         assert_eq!(parse("[' ','.','\r','\n']:iswhite").unwrap().eval_default().unwrap().to_string(), "[true, false, true, true]");
-        assert_eq!(parse("['a','A','ch','Ch','CH']:isupper").unwrap().eval_default().unwrap().to_string(), "[false, true, false, false, true]");
-        assert_eq!(parse("['a','A','ch','Ch','CH']:islower").unwrap().eval_default().unwrap().to_string(), "[true, false, true, false, false]");
+        assert_eq!(parse("['á','Á','ch','Ch','CH']:isupper").unwrap().eval_default().unwrap().to_string(), "[false, true, false, false, true]");
+        assert_eq!(parse("['á','Á','ch','Ch','CH']:islower").unwrap().eval_default().unwrap().to_string(), "[true, false, true, false, false]");
+        assert_eq!(parse("[' ','  ',\"A B\",\"A b\",['Ch'].string]:isupper").unwrap().eval_default().unwrap().to_string(), "[false, false, true, false, false]");
+        assert_eq!(parse("[' ','  ',\"a b\",['Ch'].string,['ch'].string]:islower").unwrap().eval_default().unwrap().to_string(), "[false, false, true, false, true]");
+        assert_eq!(parse("[' ','  ',\"a b\",['Ch'].string,['ch'].string]:islower").unwrap().eval_default().unwrap().to_string(), "[false, false, true, false, true]");
+        assert_eq!(parse("['0', '+', \"0\", \"+\", 0]:isnumeric").unwrap().eval_default().unwrap().to_string(), "[true, false, true, false, <!>");
+        assert_eq!(parse("[\"-123\",\"+-1\",'a'.repeat,'00',[1].string]:isnumeric").unwrap().eval_default().unwrap().to_string(), "[true, false, false, false, <!>");
     }
 }
 
@@ -80,4 +142,5 @@ pub fn init(keywords: &mut crate::keywords::Keywords) {
     keywords.insert("iswhite", eval_class);
     keywords.insert("isupper", eval_class);
     keywords.insert("islower", eval_class);
+    keywords.insert("isnumeric", eval_class);
 }
