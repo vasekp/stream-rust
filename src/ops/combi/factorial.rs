0107d8ad87995d5c7feeaa6a6bc79053732d2b48
Vašek Potoček <vasek.potocek@post.cz>
2025-11-14 14:00:59 +0100

    New ops: 'binom', 'comb', 'rcomb'

--- src/ops/combi/factorial.rs
+++ src/ops/combi/factorial.rs
@@ -1,4 +1,5 @@
 use crate::base::*;
+use crate::utils::factorial;
 
 fn eval_factorial(node: Node, env: &Env) -> Result<Item, StreamError> {
     let node = node.eval_all(env)?.resolve();
@@ -9,14 +10,84 @@ fn eval_factorial(node: Node, env: &Env) -> Result<Item, StreamError> {
             if x.is_negative() {
                 return Err(StreamError::new("input can't be negative", node));
             }
-            let Ok(x) = x.try_into() else {
+            let Some(x) = x.to_u32() else {
                 return Err(StreamError::new("input too large", node));
             };
-            Ok(Item::new_number(crate::utils::factorial(x)))
+            Ok(Item::new_number(factorial(x)))
         },
         _ => Err(StreamError::new("expected: number.factorial or factorial(number)", node))
     }
 }
+
+fn eval_binom(node: Node, env: &Env) -> Result<Item, StreamError> {
+    let node = node.eval_all(env)?.resolve();
+    match &node {
+        RNode::NoSource(RNodeNS { args: RArgs::Two(Item::Number(n), Item::Number(k)), .. })
+        => {
+            if n.is_negative() | k.is_negative() {
+                return Err(StreamError::new("input can't be negative", node));
+            }
+            let (Some(n), Some(k)) = (n.to_u32(), k.to_u32()) else {
+                return Err(StreamError::new("input too large", node));
+            };
+            if k > n {
+                return Err(StreamError::new("out of range", node));
+            }
+            Ok(Item::new_number(factorial(n) / (factorial(k) * factorial(n - k))))
+        },
+        _ => Err(StreamError::new("expected: binom(n, k)", node))
+    }
+}
+
+fn eval_comb(node: Node, env: &Env) -> Result<Item, StreamError> {
+    let node = node.eval_all(env)?;
+    try_with!(node, node.check_no_source()?);
+    let mut total = 0;
+    let mut denom = UNumber::one();
+    for arg in &node.args {
+        let Item::Number(k) = arg else {
+            return Err(StreamError::new("expected: comb(number, number, ...)", node));
+        };
+        if k.is_negative() {
+            return Err(StreamError::new("input can't be negative", node));
+        }
+        let Some(k) = k.to_u32() else {
+            return Err(StreamError::new("input too large", node));
+        };
+        total = try_with!(node, total.checked_add(&k)
+            .ok_or(BaseError::from("input too large"))?);
+        denom *= factorial(k);
+    }
+    Ok(Item::new_number(factorial(total) / denom))
+}
+fn eval_rcomb(node: Node, env: &Env) -> Result<Item, StreamError> {
+    let node = node.eval_all(env)?;
+    try_with!(node, node.check_no_source()?);
+    let mut total = 0;
+    let mut denom = UNumber::one();
+    let mut vec = Vec::new();
+    for arg in &node.args {
+        let Item::Number(k) = arg else {
+            return Err(StreamError::new("expected: comb(number, number, ...)", node));
+        };
+        if k.is_negative() {
+            return Err(StreamError::new("input can't be negative", node));
+        }
+        let Some(k) = k.to_u32() else {
+            return Err(StreamError::new("input too large", node));
+        };
+        if k == 0 { continue; }
+        if vec.contains(&k) {
+            denom *= 1 + vec.iter().filter(|x| *x == &k).count();
+        }
+        vec.push(k);
+        total = try_with!(node, total.checked_add(&k)
+            .ok_or(BaseError::from("input too large"))?);
+        denom *= factorial(k);
+    }
+    Ok(Item::new_number(factorial(total) / denom))
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -29,8 +100,51 @@ mod tests {
         test_eval!("factorial(-1)" => err);
         test_eval!("factorial(10^10)" => err);
     }
+
+    #[test]
+    fn test_binom() {
+        use super::*;
+        test_eval!("seq(0):{binom(5,#)}" : 7 => "[1, 5, 10, 10, 5, 1, <!>");
+        test_eval!("binom(0,0)" => "1");
+        test_eval!("binom(-1,0)" => err);
+        test_eval!("binom(0,-1)" => err);
+        test_eval!("binom(0,1)" => err);
+        test_eval!("seq(0):{binom(2*#,#)}" : 10 => "[1, 2, 6, 20, 70, 252, 924, 3432, 12870, 48620, ...]");
+        test_eval!("binom(100,30)" => "29372339821610944823963760");
+    }
+
+    #[test]
+    fn test_comb() {
+        use super::*;
+        test_eval!("comb(3,5) == binom(8,3)" => "true");
+        test_eval!("comb(3,5,0) == binom(8,3)" => "true");
+        test_eval!("comb()" => "1");
+        test_eval!("comb(0)" => "1");
+        test_eval!("comb(0,0)" => "1");
+        test_eval!("comb(1,1,1,1) == factorial(4)" => "true");
+        test_eval!("comb(3,3,3) == factorial(9)/factorial(3)^3" => "true");
+        test_eval!("comb(3,4,5) == factorial(12)/factorial(3)/factorial(4)/factorial(5)" => "true");
+    }
+
+    #[test]
+    fn test_rcomb() {
+        use super::*;
+        test_eval!("rcomb(3,5) == comb(3,5)" => "true");
+        test_eval!("rcomb(3,5,0) == comb(3,5,0)" => "true");
+        test_eval!("rcomb()" => "1");
+        test_eval!("rcomb(0)" => "1");
+        test_eval!("rcomb(0,0)" => "1");
+        test_eval!("rcomb(1,1,1,1)" => "1");
+        test_eval!("rcomb(3,3,3) == comb(3,3,3)/factorial(3)" => "true");
+        test_eval!("rcomb(3,4,3) == comb(3,3,4)/factorial(2)" => "true");
+        test_eval!("rcomb(3,4,5) == comb(3,4,5)" => "true");
+        test_eval!("rcomb(3,4,3,4,3) == comb(3,3,3,4,4)/2/6" => "true");
+    }
 }
 
 pub fn init(keywords: &mut crate::keywords::Keywords) {
     keywords.insert("factorial", eval_factorial);
+    keywords.insert("binom", eval_binom);
+    keywords.insert("comb", eval_comb);
+    keywords.insert("rcomb", eval_rcomb);
 }
