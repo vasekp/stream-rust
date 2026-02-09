b39bc354e61d47b6de807a4c828ee84f0e60ec43
Vašek Potoček <vasek.potocek@post.cz>
2026-02-09 17:47:42 +0100

    Prepend CLI commands with a ':'

--- src/main.rs
+++ src/main.rs
@@ -21,51 +21,53 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut sess = Session::new();
 
     while let Ok(input) = rl.readline("> ") {
-        if matches!(&input[..], "list" | "less") {
-            let Some(Item::Stream(stm)) = sess.history().last() else {
-                println!("{}", "Can only use after a stream.".red());
-                continue;
-            };
-            let mut cmd = Command::new("less")
-                .args(["-R"])
-                .stdin(Stdio::piped())
-                .stdout(Stdio::inherit())
-                .spawn()
-                .expect("Failed to run less.");
-            let mut stdin = cmd.stdin.take().expect("Failed to open stdin");
-            for (ix, item) in stm.iter().enumerate() {
-                match item {
-                    Ok(item) => {
-                        let index = format!("[{}]", ix + 1).dimmed();
-                        if writeln!(stdin, "{}  {}", index, item).is_err() {
-                            break;
+        let input = input.trim();
+        if input.as_bytes()[0] == b':' {
+            match input[1..].trim() {
+                "list" | "less" => {
+                    let Some(Item::Stream(stm)) = sess.history().last() else {
+                        println!("{}", "Can only use after a stream.".red());
+                        continue;
+                    };
+                    let mut cmd = Command::new("less")
+                        .args(["-R"])
+                        .stdin(Stdio::piped())
+                        .stdout(Stdio::inherit())
+                        .spawn()
+                        .expect("Failed to run less.");
+                    let mut stdin = cmd.stdin.take().expect("Failed to open stdin");
+                    for (ix, item) in stm.iter().enumerate() {
+                        match item {
+                            Ok(item) => {
+                                let index = format!("[{}]", ix + 1).dimmed();
+                                if writeln!(stdin, "{}  {}", index, item).is_err() {
+                                    break;
+                                }
+                            },
+                            Err(err) => {
+                                let _ = writeln!(stdin, "{}", format!("error: {err}").red());
+                                break;
+                            }
                         }
-                    },
-                    Err(err) => {
-                        let _ = writeln!(stdin, "{}", format!("error: {err}").red());
-                        break;
                     }
-                }
-            }
-            drop(stdin);
-            cmd.wait().expect("Error in wait().");
-            continue;
-        }
-        if input.len() > 8 && matches!(&input[..8], "tracing ") {
-            match &input[8..] {
-                "on" => sess.set_tracer(stream::base::tracing::TextTracer::default()),
-                "off" => sess.set_tracer(()),
+                    drop(stdin);
+                    cmd.wait().expect("Error in wait().");
+                },
+                "trace" | "trace on" | "tracing" | "tracing on" =>
+                    sess.set_tracer(stream::base::tracing::TextTracer::default()),
+                "trace off" | "tracing off" =>
+                    sess.set_tracer(()),
                 _ => eprintln!("{}", "malformed command".red()),
             }
             continue;
         }
-        match stream::parse(&input) {
+        match stream::parse(input) {
             Ok(expr) => {
                 //println!("Expr Debug: {expr:?}");
                 //println!("Expr Describe: {}", expr.describe());
                 match sess.process(expr) {
                     Ok(SessionUpdate::History(index, item)) => {
-                        println!("Item Describe: {}", item.describe());
+                        //println!("Item Describe: {}", item.describe());
                         let (s, _, err) = item.format(None, Some(80));
                         println!("{} {s}", format!("%{index}:").dimmed());
                         if let Some(err) = err {
@@ -73,21 +75,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         }
                     },
                     Ok(SessionUpdate::Globals(list)) => {
-                        print!("Globals updated: ");
+                        let mut s = "Globals updated: ".to_string();
                         let mut iter = list.into_iter();
                         if let Some(first) = iter.next() {
-                            print!("{first}");
+                            s += &first.to_string();
                         }
                         for name in iter {
-                            print!(", {name}");
+                            s += &format!(", {name}");
                         }
-                        println!();
+                        println!("{}", s.yellow());
                     },
                     Err(err) => println!("{}", format!("{err}").red())
                 }
             },
             Err(err) => {
-                err.display(&input);
+                err.display(input);
                 println!("{}", format!("{err}").red());
             }
         }
