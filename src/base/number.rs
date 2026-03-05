ef9703825c47a12eb9b934811e2f89d4d1163972
Vašek Potoček <vasek.potocek@post.cz>
2026-03-05 14:35:04 +0100

    TryCast: trait implementation

--- src/base/number.rs
+++ src/base/number.rs
@@ -1,3 +1,6 @@
+use crate::base::error::*;
+use num::Signed;
+
 /// The base type for representing all numbers in Stream. The requirement is that it allows
 /// arbitrary-precision integer arithmetics. Currently alias to BigInt, but may become an i64 with
 /// BigInt fallback in the future for better performance.
@@ -9,3 +12,74 @@ pub type UNumber = ibig::UBig;
 pub use ibig::ops::{DivRem, DivRemEuclid, RemEuclid, UnsignedAbs};
 
 pub(crate) const CACHE_LEN: usize = 100;
+
+pub(crate) trait TryCast<T>: Signed where T: IsSigned + for<'a> TryFrom<&'a Self> {
+    fn try_cast(&self) -> Result<T, StreamError> {
+        if !T::IS_SIGNED && self.is_negative() {
+            Err(StreamError::new0("value can't be negative"))
+        } else {
+            self.try_into().map_err(|_| StreamError::new0("value too large"))
+        }
+    }
+
+    fn try_cast_within(&self, range: impl std::ops::RangeBounds<T>) -> Result<T, StreamError>
+    where T: Ord + std::fmt::Display {
+        if !T::IS_SIGNED && self.is_negative() {
+            Err(StreamError::new0("value can't be negative"))
+        } else {
+            let x = self.try_into().map_err(|_| StreamError::new0("value too large"))?;
+            if range.contains(&x) {
+                Ok(x)
+            } else {
+                use std::ops::Bound::*;
+                let err = match (range.start_bound(), range.end_bound()) {
+                    (Included(min), Unbounded) => format!("must be at least {min}"),
+                    (Unbounded, Included(max)) => format!("must be at most {max}"),
+                    (Included(min), Included(max)) => format!("must be between {min} and {max}"),
+                    _ => "out of fange".into()
+                };
+                Err(StreamError::new0(err))
+            }
+        }
+    }
+}
+
+pub(crate) trait IsSigned {
+    const IS_SIGNED: bool;
+}
+
+macro_rules! impl_is_signed {
+    ($($t:ty => $v:expr),* $(,)?) => {
+        $(impl IsSigned for $t {
+            const IS_SIGNED: bool = $v;
+        })*
+    };
+}
+
+impl_is_signed!(
+    i8  => true,  i16 => true,  i32 => true,  i64 => true,  i128 => true,  isize => true,
+    u8  => false, u16 => false, u32 => false, u64 => false, u128 => false, usize => false,
+    Number => true, UNumber => false,
+);
+
+impl TryCast<u32> for Number { }
+impl TryCast<i32> for Number { }
+impl TryCast<UNumber> for Number { }
+
+pub(crate) trait TryUnsign {
+    type Unsigned;
+
+    fn try_unsign(&self) -> Result<Self::Unsigned, StreamError>;
+}
+
+impl TryUnsign for Number {
+    type Unsigned = UNumber;
+
+    fn try_unsign(&self) -> Result<Self::Unsigned, StreamError> {
+        if self.is_negative() {
+            Err(StreamError::new0("value can't be negative"))
+        } else {
+            Ok(self.unsigned_abs())
+        }
+    }
+}
