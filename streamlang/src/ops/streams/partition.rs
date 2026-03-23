5d38f0c2abddeacf24e451bc33fdb76018867ab7
Vašek Potoček <vasek.potocek@post.cz>
2026-03-23 07:19:04 +0100

    'partition': variant with stream argument

--- streamlang/src/ops/streams/partition.rs
+++ streamlang/src/ops/streams/partition.rs
@@ -2,10 +2,11 @@ use crate::base::*;
 
 fn eval_partition(node: &Node, env: &Env) -> SResult<Item> {
     let node = node.eval_all(env)?;
-    let [size] = &node.args[..] else {
-        return Err(StreamError::usage(&node.head));
+    let size = match &node.args[..] {
+        [Item::Number(size)] => Size::Fixed(size.try_cast_within(1usize..)?),
+        [Item::Stream(stm)] => Size::Stream(Rc::clone(stm)),
+        _ => return Err(StreamError::usage(&node.head)),
     };
-    let size = size.as_num()?.try_cast_within(1usize..)?;
     match node.source_checked()? {
         Item::Stream(stm) => Ok(Item::new_stream(Partition{
             head: node.head.clone(),
@@ -22,36 +23,55 @@ fn eval_partition(node: &Node, env: &Env) -> SResult<Item> {
 struct Partition<I: ItemType> {
     head: Head,
     source: Rc<dyn Stream<I>>,
-    size: usize,
+    size: Size,
+}
+
+#[derive(Clone)]
+enum Size {
+    Fixed(usize),
+    Stream(Rc<dyn Stream>),
 }
 
 impl<I: ItemType> Describe for Partition<I> {
     fn describe_inner(&self, prec: u32, env: &Env) -> String {
-        DescribeBuilder::new(&self.head, env)
-            .set_source(&self.source)
-            .push_arg(&self.size)
-            .finish(prec)
+        let mut db = DescribeBuilder::new(&self.head, env);
+        db.set_source(&self.source);
+        match &self.size {
+            Size::Fixed(size) => db.push_arg(&size),
+            Size::Stream(stm) => db.push_arg(&stm),
+        };
+        db.finish(prec)
     }
 }
 
 impl<I: ItemType> Stream for Partition<I> {
     fn to_iter(self: Rc<Self>) -> Box<dyn SIterator> {
         let iter = self.source.iter();
-        PartitionIter{size: self.size, iter, node: self}.wrap()
+        match &self.size {
+            Size::Fixed(size) => HomPartitionIter{iter, size: *size, node: self}.wrap(),
+            Size::Stream(stm) => {
+                let size_iter = stm.iter();
+                HetPartitionIter{iter, size_iter, node: self}.wrap()
+            }
+        }
     }
 
     fn len(&self) -> Length {
-        self.source.len().map(|len| (len + self.size - 1) / self.size)
+        match &self.size {
+            Size::Fixed(size) =>
+                self.source.len().map(|len| (len + size - 1) / size),
+            Size::Stream(_) => Length::Unknown,
+        }
     }
 }
 
-struct PartitionIter<I: ItemType> {
+struct HomPartitionIter<I: ItemType> {
     node: Rc<Partition<I>>,
     iter: Box<dyn SIterator<I>>,
     size: usize,
 }
 
-impl<I: ItemType> PreIterator for PartitionIter<I> {
+impl<I: ItemType> PreIterator for HomPartitionIter<I> {
     fn next(&mut self) -> SResult<Option<Item>> {
         let mut vec = Vec::with_capacity(self.size);
         for _ in 0..self.size {
@@ -82,6 +102,40 @@ impl<I: ItemType> PreIterator for PartitionIter<I> {
     }
 }
 
+struct HetPartitionIter<I: ItemType> {
+    node: Rc<Partition<I>>,
+    iter: Box<dyn SIterator<I>>,
+    size_iter: Box<dyn SIterator>,
+}
+
+impl<I: ItemType> PreIterator for HetPartitionIter<I> {
+    fn next(&mut self) -> SResult<Option<Item>> {
+        let size: usize = iter_try!(self.size_iter.next())
+            .to_num()?.try_cast()?;
+        if size.is_zero() {
+            return Ok(Some(Item::from(Vec::<I>::new())));
+        }
+        let mut vec = Vec::with_capacity(size);
+        for _ in 0..size {
+            let Some(next) = self.iter.next()? else { break; };
+            vec.push(next);
+        }
+        if vec.is_empty() {
+            Ok(None)
+        } else {
+            Ok(Some(Item::from(vec)))
+        }
+    }
+
+    fn len_remain(&self) -> Length {
+        Length::Unknown
+    }
+
+    fn origin(&self) -> &Rc<Partition<I>> {
+        &self.node
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -92,6 +146,9 @@ mod tests {
         test_eval!("seq.partition(2)" : 10 => "[[1, 2], [3, 4], [5, 6], [...], ...]");
         test_eval!("seq.partition(1)" : 10 => "[[1], [2], [3], [4], [5], ...]");
         test_eval!("seq.partition(0)" => err);
+        test_eval!("seq.partition(seq)" : 10 => "[[1], [2, 3], [4, 5, 6], [...], ...]");
+        test_eval!("seq.partition([1, 0].repeat)" => "[[1], [], [2], ...]");
+        test_eval!("\"hello\".partition([2, 1, 0])" => "[\"he\", \"l\", \"\"]");
         test_len!("(1..10).partition(3)" => 4);
         test_len!("(1..10).partition(5)" => 2);
         test_len!("(1..10).partition(9)" => 2);
@@ -108,11 +165,13 @@ mod tests {
 pub fn init(symbols: &mut crate::symbols::Symbols) {
     symbols.insert(["partition", "chunks"], eval_partition, r#"
 A stream of non-overlapping arrays from the input `stream` up to size `size`, or substrings of `string` of up to length `size`.
+`size` may be given as a number or a stream of numbers.
 = stream.?(size)
 = string.?(size)
 > ?seq.?(3) : 10 => [[1, 2, 3], [4, 5, 6], [7, ...], ...]
 > (1..5).?(2) : 10 => [[1, 2], [3, 4], [5]] ; last group may be incomplete
 > "Hello world".?(3) => ["Hel", "lo ", "wor", "ld"]
+> ?seq.?([1, 2].?repeat) : 10 => [[1], [2, 3], [4], [5, 6], ...]
 : groups
 "#);
 }
